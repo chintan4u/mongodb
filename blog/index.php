765e83b4351a3ef53d2dc5646e98451be5179c33
shaikzai <zainul.shaikh@tieto.com>
2015-01-21 17:44:12 +0530

    Updated files for assignment

--- blog/index.php
+++ blog/index.php
@@ -1,1 +1,36 @@
 <?php
+// create mongo client. 
+$client = new MongoClient();
+
+//get database.
+$db = $client->tieto;
+
+//get post collection.
+$collection = $db->post;
+
+//find post by username
+$username = isset($_GET['username']) ? $_GET['username'] : null;
+
+
+//insert mongodb code to find post by username
+
+?>
+
+<html>
+  <head></head>
+  <body>
+	<h1>Blog Posts</h1>
+	<?php if(isset($posts)):?>
+		<?php foreach($posts as $post ): ?>
+		 <div>
+		   <h3><?php echo $post['title'];?></h3>
+		   <small><?php echo "username : " . $post['username'];?></small>
+		   <p><?php echo $post['body'];?></p>
+		   <em><?php echo date("d/m/Y",$post['created_at'])?></em>
+		 </div>
+		<?php endforeach;?>
+	<?php else:?>
+	  <?php echo "No blog post found."?>
+	<?php endif;?>
+  </body>
+</html>
--- blog/post.php
+++ blog/post.php
@@ -1,1 +1,31 @@
 <?php
+
+// create mongo client. 
+$client = new MongoClient();
+
+//get database.
+$db = $client->tieto;
+
+//get post collection.
+$collection = $db->post;
+
+//save post
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+ echo "Please insert correct code here.";die;
+}
+
+
+?>
+
+<html>
+  <head></head>
+  <body>
+	<h1>Blog Post</h1>
+	<form action="" method="POST">
+	 <p>title: <input type="text" name="title"/></p>
+	 <p>post: <textarea name="body"></textarea></p>
+	 <p>username: <input type="text" name="username"/></p>
+	 <p><input type="submit" value="save"/></p>
+	</form>
+  </body>
+</html>
